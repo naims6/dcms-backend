b5ff0008acb26eb116a6e8a7bdb67815a08d4cd4
naims <naimdrive6@gmail.com>
2026-04-20 22:17:04 +0600

    initialized project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+export const test = () => {
+  console.log("hello test");
+};
+
+export default app;
