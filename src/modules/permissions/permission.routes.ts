ee669863e4f81d3536936faf79e2f5c8f790f46b
naims <naimdrive6@gmail.com>
2026-06-14 20:26:35 +0600

    feat: define permission constants.

--- src/modules/permissions/permission.routes.ts
+++ src/modules/permissions/permission.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import { permissionController } from './permission.controller.js';
+import { Router } from "express";
+import { permissionController } from "./permission.controller.js";
 
 const router: Router = Router();
 
 // get all permissions
-router.get('/get-all', permissionController.getAllPermissions);
+router.get("/get-all", permissionController.getAllPermissions);
 
 export const PermissionRoutes = router;
